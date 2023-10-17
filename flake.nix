348ed671ebfcffb9fd54bcda178ddf62de5ee6c6
Maximilian Kienitz <max@kienitz.dev>
2023-10-17 11:26:58 +0200

    feat: add nix flake for jupyter

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            alejandra
+            nil
+            (python311.withPackages (ps:
+              with ps; [
+                jupyter
+                matplotlib
+                numpy
+                scipy
+              ]))
+          ];
+        };
+      }
+    );
+}
